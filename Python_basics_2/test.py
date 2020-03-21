850e5fcfdfa94f20e29ba40926c5b7d4effe49cb
SP9144 <shreeya.pahune@research.iiit.ac.in>
2020-03-22 00:59:22 +0530

    All files added.

---
+++ Python_basics_2/test.py
@@ -0,0 +1,19 @@
+import numpy as np
+from numpy import exp as NE
+
+a=list(range(0,5))
+print(a)
+
+print("hello")
+for i in range(5):
+    print(i)
+    if i%2==0:
+        print("Even")
+    elif i%2!=0:
+        print("Odd")
+    else:
+        print(i)
+
+# Hash for commenting
+
+dict={'car': 2, 'bike': 3}
